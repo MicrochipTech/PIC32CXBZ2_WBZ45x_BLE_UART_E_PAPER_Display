dc0654fb8fdcf71a2e2e322604e83d56f58f5017
Roshan Raj A <Roshan.Raj2@microchip.com>
2023-01-02 14:22:04 +0530

    README.md

---
+++ WBZ451_E_PAPER_BLE_UART/WBZ451_E_PAPER/firmware/WBZ451_E_PAPER.X/build/default/debug/_ext/1731642671/eink_bundle_image.o.d
@@ -0,0 +1,5 @@
+build/default/debug/_ext/1731642671/eink_bundle_image.o: \
+ ../src/click_routines/eink_bundle/eink_bundle_image.c \
+ ../src/click_routines/eink_bundle/eink_bundle_image.h
+
+../src/click_routines/eink_bundle/eink_bundle_image.h:
---
+++ WBZ451_E_PAPER_BLE_UART/WBZ451_E_PAPER/firmware/WBZ451_E_PAPER.X/build/default/production/_ext/1731642671/eink_bundle_font.o.d
@@ -0,0 +1,5 @@
+build/default/production/_ext/1731642671/eink_bundle_font.o: \
+ ../src/click_routines/eink_bundle/eink_bundle_font.c \
+ ../src/click_routines/eink_bundle/eink_bundle_font.h
+
+../src/click_routines/eink_bundle/eink_bundle_font.h:
